b8a3b78a518437c98a1496869a7a205456cb6954
alx.melnichuk <alx.melnichuk@gmail.com>
2022-08-24 10:29:31 +0300

    Transfer to a level above SiteScheme.

--- src/app/lm-components/lm-components.module.ts
+++ src/app/lm-components/lm-components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatExpansionModule } from '@angular/material/expansion';
 
-import { SiteSchemeModule } from '../lib-site/components/site-scheme/site-scheme.module';
+import { SiteSchemeModule } from '../lib-site/site-scheme/site-scheme.module';
 
 import { LmComponentsComponent } from './lm-components.component';
 import { LmComponentsRoutingModule } from './lm-components-routing.module';
