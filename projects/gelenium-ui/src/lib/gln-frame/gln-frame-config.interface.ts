69cc25f99f275bc20beada30e08f565d708b7cc1
alx.melnichuk <alx.melnichuk@gmail.com>
2022-10-12 15:51:51 +0300

    Removed: GlnBaseProperties, GlnFrameExteriorInput.

--- projects/gelenium-ui/src/lib/gln-frame/gln-frame-config.interface.ts
+++ projects/gelenium-ui/src/lib/gln-frame/gln-frame-config.interface.ts
@@ -1,8 +1,3 @@
-import { GlnFrameOrnamAlign } from '../directives/gln-frame-ornament/gln-frame-ornam-align.interface';
-
-import { GlnFrameExterior } from './gln-frame-exterior.interface';
-import { GlnFrameSize } from './gln-frame-size.interface';
-
 export interface GlnFrameConfig {
   exterior?: string | undefined; // GlnFrameExteriorType
   frameSize?: string | undefined; // GlnFrameSizeType
@@ -10,17 +5,4 @@ export interface GlnFrameConfig {
   isNoAnimation?: boolean | undefined;
   isNoLabel?: boolean | undefined; // deprecate
   isRequired?: boolean | undefined;
-}
-
-// deprecate
-export interface GlnFrameConfigOld {
-  exterior?: GlnFrameExterior | undefined;
-  frameSize?: GlnFrameSize | undefined;
-  frameSizeValue?: number | undefined;
-  isLabelShrink?: boolean | undefined;
-  isNoAnimation?: boolean | undefined;
-  isNoLabel?: boolean | undefined;
-  labelPd?: number | undefined; // px
-  ornamLfAlign?: GlnFrameOrnamAlign | undefined;
-  ornamRgAlign?: GlnFrameOrnamAlign | undefined;
 }
--- projects/gelenium-ui/src/lib/public-api.ts
+++ projects/gelenium-ui/src/lib/public-api.ts
@@ -7,7 +7,6 @@ export * from './_utils/public-api';
 export * from './directives/gln-auto-focuse/public-api';
 export * from './directives/gln-color/public-api';
 export * from './directives/gln-frame-exterior-button/public-api';
-export * from './directives/gln-frame-exterior-input/public-api';
 export * from './directives/gln-frame-ornament/public-api';
 export * from './directives/gln-frame-size/public-api';
 export * from './directives/gln-regex/public-api';
