0aeaf1ee16644aa40573ab8cfbdd0ee18623c719
alx.melnichuk <alx.melnichuk@gmail.com>
2023-01-11 12:33:32 +0200

    Update the GlnAutocomplete.

--- projects/gelenium-ui/src/lib/gln-autocomplete/gln-autocomplete.module.ts
+++ projects/gelenium-ui/src/lib/gln-autocomplete/gln-autocomplete.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { GlnOptionModule } from '../gln-option/gln-option.module';
 import { GlnOptionGroupModule } from '../gln-option-group/gln-option-group.module';
-import { GlnOptionListModule } from '../gln-option-list/gln-option-list.module';
 
 import { GlnAutocompleteComponent } from './gln-autocomplete.component';
 import { GlnAutocompleteTriggerDirective } from './gln-autocomplete-trigger.directive';
@@ -12,7 +11,7 @@ import { GlnHighlightPipe } from './gln-highlight.pipe';
 
 @NgModule({
   declarations: [GlnAutocompleteComponent, GlnAutocompleteTriggerDirective, GlnAutocompletePanelDirective, GlnHighlightPipe],
-  imports: [CommonModule, GlnOptionModule, GlnOptionGroupModule, GlnOptionListModule],
+  imports: [CommonModule, GlnOptionModule, GlnOptionGroupModule],
   exports: [GlnAutocompleteComponent, GlnAutocompleteTriggerDirective, GlnAutocompletePanelDirective, GlnHighlightPipe],
 })
 export class GlnAutocompleteModule {}
