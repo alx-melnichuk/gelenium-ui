73e8fda05259cc238739b5d72c1d59006ed40393
alx.melnichuk <alx.melnichuk@gmail.com>
2023-01-28 20:21:18 +0200

    Adding a color palette to GlnPagination.

--- src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
+++ src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
@@ -5,13 +5,21 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
-import { GlnPaginationModule } from 'gelenium-ui';
+import { GlnColorModule, GlnPaginationModule } from 'gelenium-ui';
 
 import { CmPaginationBasicComponent } from './cm-pagination-basic.component';
 
 @NgModule({
   declarations: [CmPaginationBasicComponent],
-  imports: [CommonModule, ReactiveFormsModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnPaginationModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    MatExpansionModule,
+    MatTabsModule,
+    GlnColorModule,
+    GlnPaginationModule,
+  ],
   exports: [CmPaginationBasicComponent],
 })
 export class CmPaginationBasicModule {}
