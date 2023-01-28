92668114616b15b3d11c95c1315c415f10122f92
alx.melnichuk <alx.melnichuk@gmail.com>
2023-01-28 16:53:03 +0200

    Since "GlnOptionGroupModule" is not used, it is removed from: GlnAutocompleteModule, GlnSelectModule.

--- projects/gelenium-ui/src/lib/gln-autocomplete/gln-autocomplete.module.ts
+++ projects/gelenium-ui/src/lib/gln-autocomplete/gln-autocomplete.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GlnOptionModule } from '../gln-option/gln-option.module';
-import { GlnOptionGroupModule } from '../gln-option-group/gln-option-group.module';
 
 import { GlnAutocompleteComponent } from './gln-autocomplete.component';
 import { GlnAutocompleteTriggerDirective } from './gln-autocomplete-trigger.directive';
@@ -10,7 +9,7 @@ import { GlnAutocompletePanelDirective } from './gln-autocomplete-panel.directiv
 
 @NgModule({
   declarations: [GlnAutocompleteComponent, GlnAutocompleteTriggerDirective, GlnAutocompletePanelDirective],
-  imports: [CommonModule, GlnOptionModule, GlnOptionGroupModule],
+  imports: [CommonModule, GlnOptionModule],
   exports: [GlnAutocompleteComponent, GlnAutocompleteTriggerDirective, GlnAutocompletePanelDirective],
 })
 export class GlnAutocompleteModule {}
--- projects/gelenium-ui/src/lib/gln-select/gln-select.module.ts
+++ projects/gelenium-ui/src/lib/gln-select/gln-select.module.ts
@@ -7,7 +7,6 @@ import { GlnFrameModule } from '../gln-frame/gln-frame.module';
 import { GlnFrameOrnamentModule } from '../directives/gln-frame-ornament/gln-frame-ornament.module';
 import { GlnHintOrErrorModule } from '../gln-hint-or-error/gln-hint-or-error.module';
 import { GlnOptionModule } from '../gln-option/gln-option.module';
-import { GlnOptionGroupModule } from '../gln-option-group/gln-option-group.module';
 
 import { GlnSelectComponent } from './gln-select.component';
 import { GLN_SELECT_SCROLL_STRATEGY_PROVIDER_REPOSITION } from './gln-select.providers';
@@ -23,7 +22,6 @@ import { GlnSelectTriggerDirective } from './gln-select-trigger.directive';
     GlnFrameOrnamentModule,
     GlnHintOrErrorModule,
     GlnOptionModule,
-    GlnOptionGroupModule,
   ],
   exports: [GlnSelectComponent, GlnSelectTriggerDirective],
   providers: [GLN_SELECT_SCROLL_STRATEGY_PROVIDER_REPOSITION],
