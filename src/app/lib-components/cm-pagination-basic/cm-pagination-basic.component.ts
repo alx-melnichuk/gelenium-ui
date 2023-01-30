72a3c30bde337b9947fdbfadf06a3efd657f502c
alx.melnichuk <alx.melnichuk@gmail.com>
2023-01-30 21:03:20 +0200

    Update the CmPagination.

--- src/app/lib-components/cm-pagination-basic/cm-pagination-basic.component.ts
+++ src/app/lib-components/cm-pagination-basic/cm-pagination-basic.component.ts
@@ -27,17 +27,40 @@ export class CmPaginationBasicComponent {
 
   public urlCmPagination = '/' + RouterConfig.get('URL_COMPONENTS') + '/' + RouterConfig.get('URL_COMPONENTS_PAGINATION');
 
+  public isShowBasic = true;
+  public isShowAttributes = true;
+  public isShowSize = true;
+  public isShowBorder = true;
+
   // Page: "Basic" 01
   public page01a: number = 1;
   public page01b: number = 1;
 
   // Page: "Attributes" 02
-  public exterior02b = 'outlined';
+  public exterior02a = 'outlined';
   public page02a: number = 1;
   public page02b: number = 1;
   public page02c: number = 1;
   public page02d: number = 1;
-  public page02e: number = 1;
+
+  // Page: "Size" 03
+  public exterior03a = 'outlined';
+  public page03a: number = 1;
+  public page03b: number = 1;
+  public page03c: number = 1;
+  public page03d: number = 1;
+  public page03e: number = 1;
+  public page03f: number = 1;
+
+  public exterior03b = 'outlined';
+  public page03h: number = 1;
+  public page03i: number = 1;
+  public page03j: number = 1;
+
+  // Page: "Border" 04
+  public exterior04a = 'outlined';
+  public page04a: number = 1;
+  public page04b: number = 1;
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
--- src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
+++ src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -11,15 +10,7 @@ import { CmPaginationBasicComponent } from './cm-pagination-basic.component';
 
 @NgModule({
   declarations: [CmPaginationBasicComponent],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatButtonToggleModule,
-    MatExpansionModule,
-    MatTabsModule,
-    GlnColorModule,
-    GlnPaginationModule,
-  ],
+  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnColorModule, GlnPaginationModule],
   exports: [CmPaginationBasicComponent],
 })
 export class CmPaginationBasicModule {}
