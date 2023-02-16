db7ccafdfa13db0aaa8a0fbe4dd6fa1b668adfc3
alx.melnichuk <alx.melnichuk@gmail.com>
2023-02-16 10:38:11 +0200

    Updated module list for CmPaginationBasic.

--- src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
+++ src/app/lib-components/cm-pagination-basic/cm-pagination-basic.module.ts
@@ -4,13 +4,13 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
-import { GlnColorModule, GlnPaginationModule } from 'gelenium-ui';
+import { GlnPaginationModule } from 'gelenium-ui';
 
 import { CmPaginationBasicComponent } from './cm-pagination-basic.component';
 
 @NgModule({
   declarations: [CmPaginationBasicComponent],
-  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnColorModule, GlnPaginationModule],
+  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnPaginationModule],
   exports: [CmPaginationBasicComponent],
 })
 export class CmPaginationBasicModule {}
