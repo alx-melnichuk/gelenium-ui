728b043a00998b170a6ad4d77219d30d7c540344
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-02 14:12:41 +0200

    Update create GlnOverflowChild.

--- projects/gelenium-ui/src/lib/gln-tooltip/gln-overflow-child.directive.spec.ts
+++ projects/gelenium-ui/src/lib/gln-tooltip/gln-overflow-child.directive.spec.ts
@@ -1,8 +1,14 @@
+import { ChangeDetectorRef, ElementRef } from '@angular/core';
 import { GlnOverflowChildDirective } from './gln-overflow-child.directive';
+
+const testElement: HTMLElement = document.createElement('div');
+const hostRef = new ElementRef<HTMLElement>(testElement);
+
+const changeDetectorRef: ChangeDetectorRef = {} as ChangeDetectorRef;
 
 describe('GlnOverflowChildDirective', () => {
   it('should create an instance', () => {
-    const directive = new GlnOverflowChildDirective();
+    const directive = new GlnOverflowChildDirective(hostRef, changeDetectorRef);
     expect(directive).toBeTruthy();
   });
 });
