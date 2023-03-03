39cd38371b723f169bdc3537c0ec4425c19c10fb
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-03 15:49:51 +0200

    Added "cdk-scrollable" to SiteScheme to allow child elements to handle scroll events.

--- src/app/lib-site/site-scheme/site-scheme.module.ts
+++ src/app/lib-site/site-scheme/site-scheme.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { SiteSchemeComponent } from './site-scheme.component';
 
 @NgModule({
   declarations: [SiteSchemeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollingModule],
   exports: [SiteSchemeComponent],
 })
 export class SiteSchemeModule {}
