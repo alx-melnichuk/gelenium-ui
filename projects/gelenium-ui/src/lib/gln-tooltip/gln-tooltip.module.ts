e9c04d594e1ac91f4f33761c3a638e48fbc6a17b
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-10 20:38:12 +0200

    Removed default provider for GlnToolTip.

--- projects/gelenium-ui/src/lib/gln-tooltip/gln-tooltip.module.ts
+++ projects/gelenium-ui/src/lib/gln-tooltip/gln-tooltip.module.ts
@@ -4,7 +4,6 @@ import { OverlayModule } from '@angular/cdk/overlay';
 
 import { GlnTooltipComponent } from './gln-tooltip.component';
 import { GlnTooltipDirective } from './gln-tooltip.directive';
-import { GLN_TOOLTIP_SCROLL_STRATEGY_PROVIDER_CLOSE } from './gln-tooltip.providers';
 import { GlnOverflowChildDirective } from './gln-overflow-child.directive';
 
 @NgModule({
@@ -12,6 +11,5 @@ import { GlnOverflowChildDirective } from './gln-overflow-child.directive';
   imports: [CommonModule, OverlayModule],
   exports: [GlnTooltipComponent, GlnTooltipDirective, GlnOverflowChildDirective],
   entryComponents: [GlnTooltipComponent],
-  providers: [GLN_TOOLTIP_SCROLL_STRATEGY_PROVIDER_CLOSE],
 })
 export class GlnTooltipModule {}
