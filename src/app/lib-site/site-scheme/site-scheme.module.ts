4dfddc97b8f7a85da9a4248da37e0dc0ee9bca93
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-10 20:39:32 +0200

    Removed 'cdk-scrollable' directive from SiteScheme.

--- src/app/lib-site/site-scheme/site-scheme.module.ts
+++ src/app/lib-site/site-scheme/site-scheme.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { SiteSchemeComponent } from './site-scheme.component';
 
 @NgModule({
   declarations: [SiteSchemeComponent],
-  imports: [CommonModule, ScrollingModule],
+  imports: [CommonModule],
   exports: [SiteSchemeComponent],
 })
 export class SiteSchemeModule {}
