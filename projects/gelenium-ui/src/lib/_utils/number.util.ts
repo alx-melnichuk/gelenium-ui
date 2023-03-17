b54770f04d8b5609d325a96619393f7dd32049a0
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-17 15:44:33 +0200

    Added 'converInt' method for NumberUtil.

--- projects/gelenium-ui/src/lib/_utils/number.util.ts
+++ projects/gelenium-ui/src/lib/_utils/number.util.ts
@@ -5,4 +5,8 @@ export class NumberUtil {
   public static roundTo100(value: number): number {
     return Math.round(value * 100) / 100;
   }
+  public static converInt(size: string, defaultValue: number): number {
+    const sizeNum: number = Number.parseInt(size);
+    return !Number.isNaN(sizeNum) ? sizeNum : defaultValue;
+  }
 }
