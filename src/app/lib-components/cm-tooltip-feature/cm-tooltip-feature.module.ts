42b1925b298038a9ebecb668df0b4a2412a27f3d
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-22 11:21:45 +0200

    Removed "MatButtonToggleModule" from CmTooltip pages.

--- src/app/lib-components/cm-tooltip-feature/cm-tooltip-feature.module.ts
+++ src/app/lib-components/cm-tooltip-feature/cm-tooltip-feature.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -10,7 +9,7 @@ import { CmTooltipFeatureComponent } from './cm-tooltip-feature.component';
 
 @NgModule({
   declarations: [CmTooltipFeatureComponent],
-  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnTooltipModule],
+  imports: [CommonModule, MatExpansionModule, MatTabsModule, GlnTooltipModule],
   exports: [CmTooltipFeatureComponent],
 })
 export class CmTooltipFeatureModule {}
