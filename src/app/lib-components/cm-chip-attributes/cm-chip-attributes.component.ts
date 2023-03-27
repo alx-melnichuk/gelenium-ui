b9782e85b17f32546d42b4c13e6de471bdb5f832
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-27 16:43:59 +0300

    Added CmChipAttributes.

---
+++ src/app/lib-components/cm-chip-attributes/cm-chip-attributes.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+
+import { RouterConfig } from '../../lib-core/config/router-config';
+import { LABEL_CSS, LABEL_HTML, LABEL_SHOW_SOURCE, LABEL_TS } from '../../lib-core/constants';
+
+@Component({
+  selector: 'app-cm-chip-attributes',
+  templateUrl: './cm-chip-attributes.component.html',
+  styleUrls: ['./cm-chip-attributes.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CmChipAttributesComponent {
+  @Input()
+  public labelShowSource = LABEL_SHOW_SOURCE;
+  @Input()
+  public labelHtml = LABEL_HTML;
+  @Input()
+  public labelTs = LABEL_TS;
+  @Input()
+  public labelCss = LABEL_CSS;
+
+  public urlCmChip = '/' + RouterConfig.get('URL_COMPONENTS') + '/' + RouterConfig.get('URL_COMPONENTS_CHIP');
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+}
