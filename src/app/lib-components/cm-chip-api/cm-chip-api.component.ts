44e13db04b263f468676304a09a1034b5d83e82e
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-27 16:49:04 +0300

    Added CmChipApi.

---
+++ src/app/lib-components/cm-chip-api/cm-chip-api.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+import { RouterConfig } from '../../lib-core/config/router-config';
+
+@Component({
+  selector: 'app-cm-chip-api',
+  templateUrl: './cm-chip-api.component.html',
+  styleUrls: ['./cm-chip-api.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CmChipApiComponent {
+  public urlCmChip = '/' + RouterConfig.get('URL_COMPONENTS') + '/' + RouterConfig.get('URL_COMPONENTS_CHIP');
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+}
