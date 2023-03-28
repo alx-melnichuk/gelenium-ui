ca2bb1e6e730047c720d6208c7bccfbb2b454b74
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-28 10:51:45 +0300

    Update CmChipBasic.

--- src/app/lib-components/cm-chip-basic/cm-chip-basic.module.ts
+++ src/app/lib-components/cm-chip-basic/cm-chip-basic.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
