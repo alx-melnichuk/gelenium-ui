8263193d214a14f31c676043f8e0e50ca50ed567
alx.melnichuk <alx.melnichuk@gmail.com>
2023-03-30 19:32:10 +0300

    Added 'Palette', 'Palette MUI', 'Palette Bootstrap' for CmCheckbox.

--- src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.component.ts
+++ src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.component.ts
@@ -23,27 +23,37 @@ export class CmCheckboxBasicComponent {
 
   public urlCmCheckbox = '/' + RouterConfig.get('URL_COMPONENTS') + '/' + RouterConfig.get('URL_COMPONENTS_CHECKBOX');
 
+  // Page: Basic01
+  isShowBasic01 = true;
   public formGroup01: FormGroup = new FormGroup({
     model01a: new FormControl(true, []),
     model01b: new FormControl(false, []),
     model01c: new FormControl(true, []),
-    model01d: new FormControl(false, []),
+    model01d: new FormControl(true, []),
+    model01e: new FormControl(false, []),
+    model01f: new FormControl(false, []),
   });
 
-  // Page: Attributes02abcdefg
-  isShowAttributes02a = true; // true;
+  // Page: Attributes02abc
+  isShowAttributes02a = true;
   public data02a = true;
   public data02b = true;
   public formGroup02a: FormGroup = new FormGroup({
     model02a: new FormControl(true, []),
     model02b: new FormControl(true, []),
     model02c: new FormControl(true, []),
-    model02d: new FormControl(true, []),
-    model02e: new FormControl(true, []),
   });
 
+  isShowAttributes02d = true; // defg
+  public control02d = {
+    model02d: new FormControl(true, []),
+    model02e: new FormControl(true, []),
+    model02f: new FormControl(null, []),
+  };
+  public formGroup02d: FormGroup = new FormGroup(this.control02d);
+
   // Page: Attributes02hijklm
-  isShowAttributes02h = true; // true;
+  isShowAttributes02h = true;
   public formGroup02h: FormGroup = new FormGroup({
     model02h: new FormControl(true, []),
     model02i: new FormControl(false, []),
@@ -67,6 +77,8 @@ export class CmCheckboxBasicComponent {
 
   // Page: Palette04
   isShowPalette04 = true;
+  public isDisabled04a: boolean = false;
+  public isIndeterm04a: boolean = false;
   public formGroup04a: FormGroup = new FormGroup({
     model04a: new FormControl(true, []),
     model04b: new FormControl(true, []),
@@ -77,6 +89,7 @@ export class CmCheckboxBasicComponent {
   });
 
   public isDisabled04i: boolean = false;
+  public isIndeterm04i: boolean = false;
   public formGroup04i: FormGroup = new FormGroup({
     model04i: new FormControl(true, []),
     model04j: new FormControl(true, []),
@@ -86,16 +99,23 @@ export class CmCheckboxBasicComponent {
   });
 
   public isDisabled04p: boolean = false;
+  public isIndeterm04p: boolean = false;
   public formGroup04p: FormGroup = new FormGroup({
     model04p: new FormControl(true, []),
     model04q: new FormControl(true, []),
     model04r: new FormControl(true, []),
     model04s: new FormControl(true, []),
     model04t: new FormControl(true, []),
+    model04u: new FormControl(true, []),
+    model04v: new FormControl(true, []),
   });
 
   // Page: Config08a
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
+
+  log(text: string): void {
+    console.log(text);
+  }
 }
