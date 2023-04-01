d7db3710ae52c685a2f486e41fadd612fa31da04
alx.melnichuk <alx.melnichuk@gmail.com>
2023-04-01 12:23:38 +0300

    Added CmCheckboxApi.

---
+++ src/app/lib-components/cm-checkbox-api/cm-checkbox-api.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CmCheckboxApiComponent } from './cm-checkbox-api.component';
+
+@NgModule({
+  declarations: [CmCheckboxApiComponent],
+  imports: [CommonModule],
+  exports: [CmCheckboxApiComponent],
+})
+export class CmCheckboxApiModule {}
