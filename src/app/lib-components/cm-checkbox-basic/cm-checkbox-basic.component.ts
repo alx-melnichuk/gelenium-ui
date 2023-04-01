20ddd6e10301942af64dd015cf1a26a9c804860b
alx.melnichuk <alx.melnichuk@gmail.com>
2023-04-01 12:25:48 +0300

    Update CmCheckboxBasic.

--- src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.component.ts
+++ src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.component.ts
@@ -1,15 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
-import { GlnCheckboxConfig, GLN_CHECKBOX_CONFIG } from 'gelenium-ui';
-
 import { RouterConfig } from '../../lib-core/config/router-config';
 import { LABEL_CSS, LABEL_HTML, LABEL_SHOW_SOURCE, LABEL_TS } from '../../lib-core/constants';
-
-const glnCheckboxConfigDefault: GlnCheckboxConfig = {
-  isNoRipple: true,
-  position: 'start',
-};
 
 @Component({
   selector: 'app-cm-checkbox-basic',
@@ -17,7 +10,6 @@ const glnCheckboxConfigDefault: GlnCheckboxConfig = {
   styleUrls: ['./cm-checkbox-basic.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  // providers: [{ provide: GLN_CHECKBOX_CONFIG, useValue: glnCheckboxConfigDefault }],
 })
 export class CmCheckboxBasicComponent {
   @Input()
@@ -42,49 +34,8 @@ export class CmCheckboxBasicComponent {
     model01f: new FormControl(false, []),
   });
 
-  // Page: Attributes02abc
-  isShowAttributes02a = true;
-  public data02a = true;
-  public data02b = true;
-  public formGroup02a: FormGroup = new FormGroup({
-    model02a: new FormControl(true, []),
-    model02b: new FormControl(true, []),
-    model02c: new FormControl(true, []),
-  });
-
-  isShowAttributes02d = true; // defg
-  public control02d = {
-    model02d: new FormControl(true, []),
-    model02e: new FormControl(true, []),
-    model02f: new FormControl(null, []),
-  };
-  public formGroup02d: FormGroup = new FormGroup(this.control02d);
-
-  // Page: Attributes02hijklm
-  isShowAttributes02h = true;
-  public formGroup02h: FormGroup = new FormGroup({
-    model02h: new FormControl(true, []),
-    model02i: new FormControl(false, []),
-    model02j: new FormControl(true, []),
-    model02k: new FormControl(false, []),
-  });
-
-  // Page: Size03
-  isShowSize03 = true;
-  public formGroup03: FormGroup = new FormGroup({
-    model03a: new FormControl(true, []),
-    model03b: new FormControl(true, []),
-    model03c: new FormControl(true, []),
-    model03d: new FormControl(true, []),
-    model03e: new FormControl(true, []),
-    model03f: new FormControl(true, []),
-    model03g: new FormControl(true, []),
-    model03h: new FormControl(true, []),
-    model03i: new FormControl(true, []),
-  });
-
   // Page: Palette04
-  isShowPalette04 = true;
+  isShowPalette04 = false;
   public isDisabled04a: boolean = false;
   public isIndeterm04a: boolean = false;
   public formGroup04a: FormGroup = new FormGroup({
@@ -118,20 +69,6 @@ export class CmCheckboxBasicComponent {
     model04v: new FormControl(true, []),
   });
 
-  // Page: Config08
-  isShowConfig08 = true;
-  public formGroup08: FormGroup = new FormGroup({
-    model08a: new FormControl(true, []),
-    model08b: new FormControl(true, []),
-  });
-
-  // Page: Api09
-  isShowApi09 = true;
-
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
-
-  log(text: string): void {
-    console.log(text);
-  }
 }
--- src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.module.ts
+++ src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormsModule, // Contains: "[(ngModel)]"
-  ReactiveFormsModule, // Contains: FormGroup, FormControl
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -13,7 +10,7 @@ import { CmCheckboxBasicComponent } from './cm-checkbox-basic.component';
 
 @NgModule({
   declarations: [CmCheckboxBasicComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatExpansionModule, MatTabsModule, GlnCheckboxModule, GlnColorModule],
+  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, MatTabsModule, GlnCheckboxModule, GlnColorModule],
   exports: [CmCheckboxBasicComponent],
 })
 export class CmCheckboxBasicModule {}
