2cd5d46de3c6898d8632884ec89011982b4a1898
alx.melnichuk <alx.melnichuk@gmail.com>
2023-04-03 22:09:36 +0300

    Update GlnCheckbox.

--- projects/gelenium-ui/src/lib/gln-checkbox/gln-checkbox.module.ts
+++ projects/gelenium-ui/src/lib/gln-checkbox/gln-checkbox.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms'; // Required for the "formGroup" form attribute.
 
 import { GlnTouchRippleModule } from '../gln-touch-ripple/gln-touch-ripple.module';
