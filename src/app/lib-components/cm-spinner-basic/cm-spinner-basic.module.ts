7d7e8fa9533d2d7348f5757ea2853be091569140
alx.melnichuk <alx.melnichuk@gmail.com>
2023-04-14 18:50:22 +0300

    Update CmSpinnerBasicModule.

--- src/app/lib-components/cm-spinner-basic/cm-spinner-basic.module.ts
+++ src/app/lib-components/cm-spinner-basic/cm-spinner-basic.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -10,7 +9,7 @@ import { CmSpinnerBasicComponent } from './cm-spinner-basic.component';
 
 @NgModule({
   declarations: [CmSpinnerBasicComponent],
-  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnSpinnerModule],
+  imports: [CommonModule, MatExpansionModule, MatTabsModule, GlnSpinnerModule],
   exports: [CmSpinnerBasicComponent],
 })
 export class CmSpinnerBasicModule {}
