05fa68c1ea18674ae10c99b0c234d51b4216badd
alx.melnichuk <alx.melnichuk@gmail.com>
2023-04-21 19:25:03 +0300

    Added logic for displaying message type icon.

--- projects/gelenium-ui/src/lib/gln-snackbar2/gln-snackbar2-alert.component.ts
+++ projects/gelenium-ui/src/lib/gln-snackbar2/gln-snackbar2-alert.component.ts
@@ -2,8 +2,10 @@ import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@
 import { GLN_SNACKBAR2_DATA } from './gln-snackbar2-config.interface';
 import { GlnSnackbar2Ref } from './gln-snackbar2-ref';
 
+export type GlnSnackbarAlertIcon = 'error' | 'warning' | 'info' | 'success';
+
 export interface GlnSnackbar2Alert {
-  data: { message: string; action: string };
+  data: { message: string; action?: string; msgType?: string; isNoClose?: boolean };
   snackbarRef: GlnSnackbar2Ref<GlnSnackbar2Alert>;
   action: () => void;
   hasAction: boolean;
@@ -11,6 +13,7 @@ export interface GlnSnackbar2Alert {
 
 @Component({
   selector: 'gln-snackbar2-alert',
+  exportAs: 'glnSnackbar2Alert',
   templateUrl: './gln-snackbar2-alert.component.html',
   styleUrls: ['./gln-snackbar2-alert.component.scss'],
   encapsulation: ViewEncapsulation.None,
@@ -18,32 +21,42 @@ export interface GlnSnackbar2Alert {
 })
 export class GlnSnackbar2AlertComponent implements GlnSnackbar2Alert {
   /** Data that was injected into the snack bar. */
-  public data: { message: string; action: string };
+  public data: { message: string; action?: string; msgType?: string; isNoClose?: boolean };
   public hasError: boolean = false;
   public hasWarning: boolean = false;
   public hasInfo: boolean = false;
   public hasSuccess: boolean = false;
-
-  constructor(
-    // { provide: GlnSnackbar2Ref, useValue: snackbarRef },
-    // { provide: GLN_SNACKBAR2_DATA, useValue: config.data },
-
-    public snackbarRef: GlnSnackbar2Ref<GlnSnackbar2AlertComponent>,
-    @Inject(GLN_SNACKBAR2_DATA) data: any
-  ) {
-    this.data = data;
-    console.log(`GlnSnackbar2Alert() data=`, data); // #
-  }
-
-  /** Performs the action on the snack bar. */
-  public action(): void {
-    console.log(`GlnSnackbar2Alert.action()`); // #
-    this.snackbarRef.dismissWithAction();
-  }
+  public hasClose: boolean = false;
 
   /** If the action button should be shown. */
   public get hasAction(): boolean {
     return !!this.data.action;
   }
   public set hasAction(value: boolean) {}
+
+  constructor(
+    // { provide: GlnSnackbar2Ref, useValue: snackbarRef },
+    // { provide: GLN_SNACKBAR2_DATA, useValue: config.data },
+    public snackbarRef: GlnSnackbar2Ref<GlnSnackbar2AlertComponent>,
+    @Inject(GLN_SNACKBAR2_DATA) data: any
+  ) {
+    this.data = data;
+    this.hasError = data.msgType === 'error';
+    this.hasWarning = data.msgType === 'warning';
+    this.hasInfo = data.msgType === 'info';
+    this.hasSuccess = data.msgType === 'success';
+    this.hasClose = !data.isNoClose;
+    console.log(`GlnSnackbar2Alert() data=`, data); // #
+  }
+
+  /** Performs the action on the snack bar. */
+  public action(): void {
+    console.log(`GlnSnackbar2Alert.action(${this.data.action})`); // #
+    this.snackbarRef.close(this.data.action);
+  }
+
+  public close(): void {
+    console.log(`GlnSnackbar2Alert.action()`); // #
+    this.snackbarRef.close();
+  }
 }
--- projects/gelenium-ui/src/lib/gln-snackbar2/gln-snackbar2-config.interface.ts
+++ projects/gelenium-ui/src/lib/gln-snackbar2/gln-snackbar2-config.interface.ts
@@ -4,18 +4,18 @@ import { InjectionToken, ViewContainerRef } from '@angular/core';
 // export type GlnSnackbarDataType = { message?: string; action?: string }; // ??
 
 /** Injection token that can be used to access the data that was passed in to a snack bar. */
-export const GLN_SNACKBAR2_DATA = new InjectionToken<unknown>('GLN_SNACKBAR2_DATA');
+export const GLN_SNACKBAR2_DATA = new InjectionToken<any>('GLN_SNACKBAR2_DATA');
 
 /** Possible values for horizontalPosition on MatSnackBarConfig. */
 export type GlnSnackbarHorizontal = 'start' | 'center' | 'end' | 'left' | 'right';
 
-/** Possible values for verticalPosition on MatSnackBarConfig. */
+/** Possible values for vertical position on MatSnackBarConfig. */
 export type GlnSnackbarVertical = 'top' | 'center' | 'bottom';
 
 /**
  * Configuration used when opening a snack-bar.
  */
-export class GlnSnackbar2Config<D = unknown> {
+export class GlnSnackbar2Config<D = any> {
   /** Data being injected into the child component. */
   data?: D | null = null;
   /** Text layout direction for the snack bar. */
@@ -23,14 +23,11 @@ export class GlnSnackbar2Config<D = unknown> {
   /** The length of time in milliseconds to wait before automatically dismissing the snack bar. */
   duration?: number = 0;
   /** The horizontal position to place the snack bar. */
-  horizontalPosition?: GlnSnackbarHorizontal = 'center';
+  horizontal?: GlnSnackbarHorizontal = 'center';
   /** Extra CSS classes to be added to the snack bar container. */
   panelClass?: string | string[];
   /** The vertical position to place the snack bar. */
-  verticalPosition?: GlnSnackbarVertical = 'bottom';
-  /**
-   * The view container that serves as the parent for the snackbar for the purposes of dependency
-   * injection. Note: this does not affect where the snackbar is inserted in the DOM.
-   */
+  vertical?: GlnSnackbarVertical = 'bottom';
+  /** The view container that serves as the parent of the dependency injection snack bar. */
   viewContainerRef?: ViewContainerRef;
 }
