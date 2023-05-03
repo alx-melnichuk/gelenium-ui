94328168e9612f6fb6a25e4d44b602b36e6a8d57
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-03 12:00:51 +0300

    Removed old version of GlnSnackbar2.

--- projects/gelenium-ui/src/lib/public-api.ts
+++ projects/gelenium-ui/src/lib/public-api.ts
@@ -25,7 +25,6 @@ export * from './gln-radio-button/public-api';
 export * from './gln-radio-group/public-api';
 export * from './gln-select/public-api';
 export * from './gln-snackbar/public-api';
-export * from './gln-snackbar2/public-api';
 export * from './gln-spinner/public-api';
 export * from './gln-switch/public-api';
 export * from './gln-textarea/public-api';
