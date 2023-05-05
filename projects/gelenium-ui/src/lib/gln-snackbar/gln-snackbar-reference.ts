50bcf663326fd1f6ac539d26d74a2f370a5ddbe1
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-05 19:23:49 +0300

    The "openContent" method returns a truncated copy of GlnSnackbarRef.

--- projects/gelenium-ui/src/lib/gln-snackbar/gln-snackbar-reference.ts
+++ projects/gelenium-ui/src/lib/gln-snackbar/gln-snackbar-reference.ts
@@ -1,62 +1,73 @@
 import { EmbeddedViewRef } from '@angular/core';
 
-export interface GlnSnackbarRef<T> {
-  instance: T | EmbeddedViewRef<any>;
-  readonly result: Promise<any>;
-  close(resultAction?: any): void;
-  dismiss(options?: { noAnimation?: boolean }): void;
+export class GlnSnackbarRef<T> {
+  /** The instance of the component making up the content of the snack bar. */
+  public instance!: T | EmbeddedViewRef<any>;
+  /** A promise that is resolved when closed (by an action or close button) and rejected when closed by a timeout. */
+  public readonly result: Promise<any> = new Promise((resolve) => {
+    this.resolve = resolve;
+  });
+  private resolve: (result: any) => void = () => {};
+
+  constructor() {}
+
+  /** Closes the snack bar by clicking on the "action" or "close" button. */
+  public close(resultAction?: any): void {
+    this.resolve(resultAction);
+  }
+  /** Closes the snack bar when the timeout expires. */
+  public dismiss(options?: { noAnimation?: boolean }): void {
+    this.resolve(undefined);
+  }
 }
 
 /** Maximum amount of milliseconds that can be passed into setTimeout. */
 const MAX_TIMEOUT = Math.pow(2, 31) - 1;
 
-export class GlnSnackbarReference<T> {
-  /** The instance of the component making up the content of the snack bar. */
-  public instance!: T;
-
-  /** A promise that is resolved when closed (by an action or close button) and rejected when closed by a timeout. */
-  public readonly result: Promise<any>;
-
+export class GlnSnackbarReference<T> extends GlnSnackbarRef<T> {
   private hideWrapperFn: () => void = () => {};
   private removeWrapperFn: () => void = () => {};
-  private resolve: (result: any) => void = () => {};
-
   /** Identifier of the time to wait before closing (the result of calling setTimeout). */
   private timeoutId: number | undefined;
 
   constructor(public readonly id: number, public readonly duration: number | undefined | null) {
-    this.result = new Promise((resolve) => {
-      this.resolve = resolve;
-    });
+    super();
     if (!!this.duration && this.duration > 0) {
-      console.log(`$setTimeout(() => { this.dismiss(); }`); // #
       this.timeoutId = window.setTimeout(() => this.dismiss(), Math.min(this.duration, MAX_TIMEOUT));
     }
   }
 
-  public setHideWrapperFn(fn: () => void): void {
+  public setHideWrapperFn = (fn: () => void): void => {
     this.hideWrapperFn = fn;
-  }
-  public setRemoveWrapperFn(fn: () => void): void {
+  };
+
+  public setRemoveWrapperFn = (fn: () => void): void => {
     this.removeWrapperFn = fn;
-  }
-  /** Closes the snack bar by clicking on the "action" or "close" button. */
-  public close(resultAction?: any): void {
-    console.log(`$close(); resolve(${resultAction});`); // #
+  };
+
+  public override close = (resultAction?: any): void => {
     this.clearTimeout();
-    this.resolve(resultAction);
+    super.close(resultAction);
     this.hideWrapperFn();
-  }
-  /** Closes the snack bar when the timeout expires. */
-  public dismiss(options?: { noAnimation?: boolean }): void {
-    console.log(`$dismiss(); resolve(undefined);`); // #
+  };
+
+  public override dismiss = (options?: { noAnimation?: boolean }): void => {
     this.clearTimeout();
-    this.resolve(undefined);
+    super.dismiss(options);
     if (!options?.noAnimation) {
       this.hideWrapperFn();
     } else {
       this.removeWrapperFn();
     }
+  };
+
+  public getGlnSnackbarRef(): GlnSnackbarRef<T> {
+    return {
+      instance: this.instance,
+      result: this.result,
+      close: (resultAction?: any): void => this.close(resultAction),
+      dismiss: (options?: { noAnimation?: boolean }): void => this.dismiss(options),
+    } as GlnSnackbarRef<T>;
   }
 
   // ** Private methods **
