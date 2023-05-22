c9677842f8a1126b16f7b2ce915cd73c4c8c9823
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-22 18:51:11 +0300

    Added 'getPathname' method to ScrollAfterRoutingUtil.

--- src/app/lib-core/utils/scroll-after-routing.util.ts
+++ src/app/lib-core/utils/scroll-after-routing.util.ts
@@ -2,31 +2,37 @@ import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 export class ScrollAfterRoutingUtil {
-  private static pathNameOld = '';
+  private static previousHref = '';
   public static listenForRouterEvents(router: Router): Subscription | null {
     if (!router) {
       return null;
     }
     return router.events.subscribe((value) => {
-      const pathName = location.pathname;
       if (value instanceof NavigationStart) {
-        ScrollAfterRoutingUtil.pathNameOld = pathName;
+        ScrollAfterRoutingUtil.previousHref = ScrollAfterRoutingUtil.getWithoutFragment(window.location.href);
       }
-      if (value instanceof NavigationEnd && pathName === ScrollAfterRoutingUtil.pathNameOld) {
-        const fragmentIdx = value.urlAfterRedirects.lastIndexOf('#');
-        if (fragmentIdx >= 0 && fragmentIdx < value.urlAfterRedirects.length - 1) {
-          const fragment = value.urlAfterRedirects.substring(fragmentIdx + 1);
-          document.getElementById(fragment)?.scrollIntoView();
+
+      if (value instanceof NavigationEnd) {
+        const locationHref = ScrollAfterRoutingUtil.getWithoutFragment(window.location.href);
+        if (locationHref === ScrollAfterRoutingUtil.previousHref) {
+          ScrollAfterRoutingUtil.scrollByFragmentFromPath();
         }
       }
     });
   }
+  public static getPathname(): string {
+    const hash = window.location.hash;
+    return ScrollAfterRoutingUtil.getWithoutFragment('#' == hash[0] ? hash.substring(1) : hash);
+  }
+  public static getWithoutFragment(value: string): string {
+    const fragmentIdx = value.lastIndexOf('#');
+    return fragmentIdx > -1 ? value.substring(0, fragmentIdx) : value;
+  }
   public static scrollByFragmentFromPath(): void {
     const pathHash = location.hash;
-    if (pathHash && pathHash[0] === '#') {
-      const idx = pathHash.indexOf('?');
-      const idxEnd = idx > -1 ? idx : pathHash.length;
-      const fragment = pathHash.substring(1, idxEnd);
+    const fragmentIdx = pathHash.lastIndexOf('#');
+    if (fragmentIdx > -1) {
+      const fragment = pathHash.substring(fragmentIdx + 1);
       document.getElementById(fragment)?.scrollIntoView();
     }
   }
