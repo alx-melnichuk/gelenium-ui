da6deaaeafef17518a6abde3c3b56525e3bcb89d
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-23 12:10:50 +0300

    Added route parameter "baseRef".

--- src/app/lib-directives/dr-regex-check-basic/dr-regex-check-basic.component.ts
+++ src/app/lib-directives/dr-regex-check-basic/dr-regex-check-basic.component.ts
@@ -35,7 +35,8 @@ export class DrRegexCheckBasicComponent {
   @Input()
   public labelCss = LABEL_CSS;
 
-  public urlDrRegexCheck = '/' + RouterConfig.get('URL_DIRECTIVES') + '/' + RouterConfig.get('URL_DIRECTIVES_REGEX_CHECK');
+  public baseRef: string = RouterConfig.get('BASE_REF');
+  public urlDrRegexCheck = this.baseRef + '/' + RouterConfig.get('URL_DIRECTIVES') + '/' + RouterConfig.get('URL_DIRECTIVES_REGEX_CHECK');
 
   public control01a = {
     model01a: new FormControl('1', []),
