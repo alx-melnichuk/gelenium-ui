f627272e2f9829dd48b870ef3f6e5e95f3aebee8
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-24 19:57:38 +0300

    Added GlnCalendar.

---
+++ projects/gelenium-ui/src/lib/gln-datepicker/gln-calendar.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'gln-calendar',
+  exportAs: 'glnCalendar',
+  templateUrl: './gln-calendar.component.html',
+  styleUrls: ['./gln-calendar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GlnCalendarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
