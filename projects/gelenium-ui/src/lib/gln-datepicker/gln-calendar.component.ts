ca39f2a1e74c727d938d7af1dfee2d6cc99291ad
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-25 20:48:59 +0300

    Layout development for GlnCalendar.

--- projects/gelenium-ui/src/lib/gln-datepicker/gln-calendar.component.ts
+++ projects/gelenium-ui/src/lib/gln-datepicker/gln-calendar.component.ts
@@ -9,7 +9,24 @@ import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GlnCalendarComponent implements OnInit {
-  constructor() {}
+  public daysOfWeek: string[] = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  public week1: string[] = [];
+  public week2: string[] = [];
+  public week3: string[] = [];
+  public week4: string[] = [];
+  public week5: string[] = [];
+
+  constructor() {
+    this.week1.push(...['', '1', '2', '3', '4', '5', '6']);
+    this.week2.push(...['7', '8', '9', '10', '11', '12', '13']);
+    this.week3.push(...['14', '15', '16', '17', '18', '19', '20']);
+    this.week4.push(...['21', '22', '23', '24', '25', '26', '27']);
+    this.week5.push(...['28', '29', '30', '31', '', ' ', ' ']);
+  }
 
   ngOnInit(): void {}
+
+  public trackByIndex(index: number): number {
+    return index;
+  }
 }
