1ae59a1bdd3d69d092f59e7cde0ac3f22509fde6
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-31 21:04:36 +0300

    PlCalendarBasic update.

--- src/app/lib-palette/pl-calendar-basic/pl-calendar-basic.module.ts
+++ src/app/lib-palette/pl-calendar-basic/pl-calendar-basic.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
@@ -10,7 +9,7 @@ import { PlCalendarBasicComponent } from './pl-calendar-basic.component';
 
 @NgModule({
   declarations: [PlCalendarBasicComponent],
-  imports: [CommonModule, MatButtonToggleModule, MatExpansionModule, MatTabsModule, GlnDatepickerModule, GlnColorModule],
+  imports: [CommonModule, MatExpansionModule, MatTabsModule, GlnDatepickerModule, GlnColorModule],
   exports: [PlCalendarBasicComponent],
 })
 export class PlCalendarBasicModule {}
