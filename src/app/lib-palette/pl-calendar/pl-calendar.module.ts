c00bcf725c1120337745a542cb7b720c97898d82
alx.melnichuk <alx.melnichuk@gmail.com>
2023-05-31 21:05:09 +0300

    PlCalendar update.

--- src/app/lib-palette/pl-calendar/pl-calendar.module.ts
+++ src/app/lib-palette/pl-calendar/pl-calendar.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlCalendarBasicModule } from '../pl-calendar-basic/pl-calendar-basic.module';
+import { PlCalendarBootstrapModule } from '../pl-calendar-bootstrap/pl-calendar-bootstrap.module';
 
 import { PlCalendarComponent } from './pl-calendar.component';
 import { PlCalendarRoutingModule } from './pl-calendar-routing.module';
 
 @NgModule({
   declarations: [PlCalendarComponent],
-  imports: [CommonModule, PlCalendarBasicModule, PlCalendarRoutingModule],
+  imports: [CommonModule, PlCalendarBasicModule, PlCalendarBootstrapModule, PlCalendarRoutingModule],
   exports: [PlCalendarComponent],
 })
 export class PlCalendarModule {}
