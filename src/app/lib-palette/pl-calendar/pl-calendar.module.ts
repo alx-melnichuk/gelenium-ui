1f385e0e8327c662ec0fb487bb58564b89039038
alx.melnichuk <alx.melnichuk@gmail.com>
2023-06-01 22:42:51 +0300

    Corrections have been made.

--- src/app/lib-palette/pl-calendar/pl-calendar.module.ts
+++ src/app/lib-palette/pl-calendar/pl-calendar.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PlCalendarBasicModule } from '../pl-calendar-basic/pl-calendar-basic.module';
 import { PlCalendarBootstrapModule } from '../pl-calendar-bootstrap/pl-calendar-bootstrap.module';
+import { PlCalendarMaterialUiModule } from '../pl-calendar-material-ui/pl-calendar-material-ui.module';
 
 import { PlCalendarComponent } from './pl-calendar.component';
 import { PlCalendarRoutingModule } from './pl-calendar-routing.module';
 
 @NgModule({
   declarations: [PlCalendarComponent],
-  imports: [CommonModule, PlCalendarBasicModule, PlCalendarBootstrapModule, PlCalendarRoutingModule],
+  imports: [CommonModule, PlCalendarBasicModule, PlCalendarBootstrapModule, PlCalendarMaterialUiModule, PlCalendarRoutingModule],
   exports: [PlCalendarComponent],
 })
 export class PlCalendarModule {}
