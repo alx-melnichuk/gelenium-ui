49d00cfc5a4a7a607a224ca0c44df08322f3e5ca
alx.melnichuk <alx.melnichuk@gmail.com>
2023-06-02 22:09:38 +0300

    Style and template optimization for GlnCalendar.

--- projects/gelenium-ui/src/lib/gln-datepicker/gln-calendar.component.ts
+++ projects/gelenium-ui/src/lib/gln-datepicker/gln-calendar.component.ts
@@ -18,7 +18,7 @@ import { GlnCalendarConfig } from './gln-calendar-config.interface';
 
 const CELL_SIZE: { [key: string]: number } = { short: 28, small: 32, middle: 36, wide: 40, large: 44, huge: 48 };
 
-const CSS_PROP_CELL_SIZE = '--glncn--cell-size';
+const CSS_PROP_CELL_SIZE = '--glncn--item-size';
 const CSS_PROP_FONT_SIZE_HEADER = '--glncnh--fn-sz';
 
 interface CalendarCell {
@@ -69,6 +69,7 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
 
   public cellSizeVal: number | null = null; // Binding attribute "cellSize".
   public currConfig: any; // GlnDatepickerConfig;
+  public isDisabledVal: boolean | null = null; // Binding attribute "isDisabled".
   public isHideOldDaysVal: boolean | null = null; // Binding attribute "isHideOldDays".
   public isHorizontVal: boolean | null = null; // Binding attribute "isHorizont".
   public nameMonth: string = '';
@@ -152,11 +153,20 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
     }
   }
 
+  // ** Public methods **
+
   public trackByIndex(index: number): number {
     return index;
   }
 
-  // Private methods
+  public clickSelectItem(cell: CalendarCell | null): void {
+    if (this.isDisabledVal || !cell || (!cell.isCurrMonth && this.isHideOldDaysVal)) {
+      console.log(`clickSelectItem() return;`); // #
+      return;
+    }
+  }
+
+  // ** Private methods **
 
   private prepareData(date: Date, weekdayLen: number): void {
     this.nameYear = this.getInfoForYear(date);
@@ -185,11 +195,13 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
     }
     return weekdayNum;
   }
-  // weekday?: number | string | undefined; // number (1, 2, 3, -1), 'narrow'-(T), 'short'-(Thu), 'long'-(Thursday)
-  private getListDaysOfWeek(weekdayLen: number): string[] {
+  /** Get a list of days of the week.
+   * @param weekday: number; 1-'narrow'(T); 2,3-'short'(Thu); -1-'long'(Thursday);
+   */
+  private getListDaysOfWeek(weekdayLen: number, date?: Date | undefined): string[] {
     const result: string[] = ['', '', '', '', '', '', ''];
     const weekdayRes: 'long' | 'short' | 'narrow' = weekdayLen <= 0 || 3 < weekdayLen ? 'long' : 1 === weekdayLen ? 'narrow' : 'short';
-    const currentDate: Date = new Date();
+    const currentDate: Date = date || new Date();
     const current: Date = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
     for (let i = 1; i < 8; i++) {
       current.setDate(i);
@@ -199,7 +211,9 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
     }
     return result;
   }
-
+  /** Get a list of days in the specified month.
+   * @param date: Date; Specifies the month and year.
+   */
   private getListDaysOfMonth(date: Date): CalendarCell[] {
     const result: CalendarCell[] = [];
     const yearInit: number = date.getFullYear();
@@ -218,7 +232,6 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
       const year: number = dateItem.getFullYear();
       const month: number = dateItem.getMonth();
       const day: number = dateItem.getDate();
-
       const isToday: boolean | undefined = year === yearInit && month === monthInit && day === dayInit ? true : undefined;
       result.push({ year, month, day, isCurrMonth: month === monthInit, isToday });
       // console.log(`calendarCellBuff.push(idx:${idx} { year: ${year}, month: ${month}, day: ${day} }`); // #
@@ -241,86 +254,6 @@ export class GlnCalendarComponent implements OnChanges, OnInit {
     this.contentWeek4 = { dataWeek: this.week4Buff };
     this.contentWeek5 = { dataWeek: this.week5Buff };
     this.contentWeek6 = { dataWeek: this.week6Buff };
-  }
-
-  private getDetailsDaysOfMonth(dateInit: Date): void {
-    /*
-    const yearInit: number = dateInit.getFullYear();
-    const monthInit: number = dateInit.getMonth();
-    const dayInit: number = dateInit.getDate();
-
-    const dateItem: Date = new Date(yearInit, monthInit, 1);
-    let indexDayOfWeeek: number = dateItem.getDay(); // 0-вс,1-пн,2-вт,3-ср,4-чт,5-пт,6-сб
-    // console.log(`dateItem.getDay()=`, dateItem.getDay());
-    dateItem.setDate(dateItem.getDate() - indexDayOfWeeek - 1);
-    // console.log(`dateItem2.getDay()=`, dateItem.getDay());
-
-    const cellBuffList: CalendarCell[] = [];
-    let idx: number = 0;
-    while (idx < 42) {
-      dateItem.setDate(dateItem.getDate() + 1);
-      const year: number = dateItem.getFullYear();
-      const month: number = dateItem.getMonth();
-      const day: number = dateItem.getDate();
-
-      const isToday: boolean | undefined = year === yearInit && month === monthInit && day === dayInit ? true : undefined;
-      cellBuffList.push({ year, month, day, isCurrMonth: month === monthInit, isToday });
-      // console.log(`calendarCellBuff.push(idx:${idx} { year: ${year}, month: ${month}, day: ${day} }`); // #
-      idx++;
-    }
-
-    this.week1Buff = cellBuffList.slice(0, 7);
-    this.week2Buff = cellBuffList.slice(7, 14);
-    this.week3Buff = cellBuffList.slice(14, 21);
-    this.week4Buff = cellBuffList.slice(21, 28);
-    this.week5Buff = cellBuffList.slice(28, 35);
-    this.week6Buff = cellBuffList.slice(35, 42);
-    */
-    /*
-    const buff: string[] = [];
-
-    // const delta: number = this.getIsWeekStartsOnMonday();
-    let indexDayOfWeeek: number = dateStart.getDay();
-    console.log(`dateStart.getDay()=`, dateStart.getDay());
-    let idx1 = 0;
-    while (indexDayOfWeeek > 0) {
-      buff.push('');
-      indexDayOfWeeek--;
-      idx1++;
-    }
-    console.log(`idx1=`, idx1);
-
-    let idxDay: number = 1;
-    const maxIdxDay: number = dateEnd.getDate() - dateStart.getDate() + 1;
-    // let dateIdxDay: Date = new Date(yearOfInit, monthOfInit, 1);
-
-    while (idxDay <= maxIdxDay) {
-      buff.push(idxDay.toString());
-      idxDay++;
-    }
-
-    let idx2 = 0;
-    while (buff.length < 42) {
-      buff.push('');
-      idx2++;
-    }
-    console.log(`idx2=`, idx2);
-
-    console.log(`buff=`, buff);
-
-    this.week1 = buff.slice(0, 7);
-    this.week2 = buff.slice(7, 14);
-    this.week3 = buff.slice(14, 21);
-    this.week4 = buff.slice(21, 28);
-    this.week5 = buff.slice(28, 35);
-    this.week6 = buff.slice(35, 42);
-    console.log(`this.week1=`, this.week1);
-    console.log(`this.week2=`, this.week2);
-    console.log(`this.week3=`, this.week3);
-    console.log(`this.week4=`, this.week4);
-    console.log(`this.week5=`, this.week5);
-    console.log(`this.week6=`, this.week6);
-    */
   }
 
   private getIsWeekStartsOnMonday(): boolean {
