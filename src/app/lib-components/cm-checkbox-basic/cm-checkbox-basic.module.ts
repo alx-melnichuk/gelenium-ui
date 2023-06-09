ea3216efbb9db22d665da8230955b6be0cb6f83f
alx.melnichuk <alx.melnichuk@gmail.com>
2023-06-09 16:46:44 +0300

    Update for CmCheckboxBasicModule.

--- src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.module.ts
+++ src/app/lib-components/cm-checkbox-basic/cm-checkbox-basic.module.ts
@@ -4,13 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 
-import { GlnCheckboxModule, GlnColorModule } from 'gelenium-ui'; // GlnColorModule only for page: Palette.
+import { GlnCheckboxModule } from 'gelenium-ui';
 
 import { CmCheckboxBasicComponent } from './cm-checkbox-basic.component';
 
 @NgModule({
   declarations: [CmCheckboxBasicComponent],
-  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, MatTabsModule, GlnCheckboxModule, GlnColorModule],
+  imports: [CommonModule, ReactiveFormsModule, MatExpansionModule, MatTabsModule, GlnCheckboxModule],
   exports: [CmCheckboxBasicComponent],
 })
 export class CmCheckboxBasicModule {}
