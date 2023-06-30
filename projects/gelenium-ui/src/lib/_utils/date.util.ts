e2ae1b3c458a0cbbf88c04478a394134cccde77e
alx.melnichuk <alx.melnichuk@gmail.com>
2023-06-30 21:09:16 +0300

    Minor changes for DateUtil.

--- projects/gelenium-ui/src/lib/_utils/date.util.ts
+++ projects/gelenium-ui/src/lib/_utils/date.util.ts
@@ -1,9 +1,11 @@
 export class DateUtil {
   /** Compare two dates (d1 > d2 = -1; d1 == d2 = 0; d1 < d2 = 1;) */
   public static compare(date1: Date | null | undefined, date2: Date | null | undefined): number {
-    let result: number = -1;
+    let result: number = 0;
     if (date1 == null && date2 != null) {
       result = 1;
+    } else if (date1 != null && date2 == null) {
+      result = -1;
     } else if (date1 != null && date2 != null) {
       const num1: number = date1.getFullYear() * 10000 + date1.getMonth() * 100 + date1.getDate();
       const num2: number = date2.getFullYear() * 10000 + date2.getMonth() * 100 + date2.getDate();
