2c78cd2f3278639a3a3ff796b41668043965437e
alx.melnichuk <alx.melnichuk@gmail.com>
2023-06-30 21:09:59 +0300

    GlnCalendarChange removal.

--- projects/gelenium-ui/src/lib/gln-calendar/gln-calendar-change.interface.ts
+++
@@ -1,8 +0,0 @@
-export const CALENDAR_VIEW_DAY = 'day';
-export const CALENDAR_VIEW_MONTH = 'month';
-export const CALENDAR_VIEW_YEAR = 'year';
-
-export interface GlnCalendarChange {
-  changed: 'year' | 'month' | 'day';
-  value: Date | null;
-}
