e6b02d4c7a20f1b9c918e6ea9c58dcf334a13e60
alx.melnichuk <alx.melnichuk@gmail.com>
2023-07-01 14:50:06 +0300

    Added ChangeUtil.

--- projects/gelenium-ui/src/lib/_utils/change.util.ts
+++ projects/gelenium-ui/src/lib/_utils/change.util.ts
@@ -1,6 +1,6 @@
 import { SimpleChange } from '@angular/core';
 
-export class SimpleChangeUtil {
+export class ChangeUtil {
   /** Check for the presence of the property in both the current and previous versions of the value. */
   public static check(simpleChange: SimpleChange | undefined, property: string): boolean {
     return !!simpleChange?.previousValue?.[property] || !!simpleChange?.currentValue?.[property];
--- projects/gelenium-ui/src/lib/_utils/public-api.ts
+++ projects/gelenium-ui/src/lib/_utils/public-api.ts
@@ -1,5 +1,6 @@
 export * from './array.util';
 export * from './boolean.util';
+export * from './change.util';
 export * from './date.util';
 export * from './event-listener.util';
 export * from './html-convert.util';
