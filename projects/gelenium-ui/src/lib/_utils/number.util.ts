4618332398608e42e57af7f0c261c932c09afd95
alx.melnichuk <alx.melnichuk@gmail.com>
2023-07-26 19:03:35 +0300

    Minor changes for NumberUtil.

--- projects/gelenium-ui/src/lib/_utils/number.util.ts
+++ projects/gelenium-ui/src/lib/_utils/number.util.ts
@@ -5,9 +5,9 @@ export class NumberUtil {
   public static roundTo100(value: number): number {
     return Math.round(value * 100) / 100;
   }
-  public static converInt(size: string, defaultValue: number): number {
-    const sizeNum: number = Number.parseInt(size);
-    return !Number.isNaN(sizeNum) ? sizeNum : defaultValue;
+  public static converInt(value: string, defaultValue: number): number {
+    const valueNum: number = Number.parseInt(value);
+    return !Number.isNaN(valueNum) ? valueNum : defaultValue;
   }
   public static getDigit(value: number, size: number = 2): string {
     return ('0'.repeat(size) + value.toString()).slice(-size);
