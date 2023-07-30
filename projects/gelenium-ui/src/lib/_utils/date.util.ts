d1a944f9296d33dd5b0dd5a934f41f371202f910
alx.melnichuk <alx.melnichuk@gmail.com>
2023-07-30 18:03:15 +0300

    Reworked date comparison methods for DateUtil.

--- projects/gelenium-ui/src/lib/_utils/date.util.ts
+++ projects/gelenium-ui/src/lib/_utils/date.util.ts
@@ -1,17 +1,23 @@
 export class DateUtil {
   /** Compare two dates (d1 < d2 = -1; d1 == d2 = 0; d1 > d2 = 1;) */
   public static compare(date1: Date | null | undefined, date2: Date | null | undefined): number {
-    let result: number = 0;
-    if (date1 == null && date2 != null) {
-      result = -1;
-    } else if (date1 != null && date2 == null) {
-      result = 1;
-    } else if (date1 != null && date2 != null) {
-      const num1: number = date1.getFullYear() * 10000 + date1.getMonth() * 100 + date1.getDate();
-      const num2: number = date2.getFullYear() * 10000 + date2.getMonth() * 100 + date2.getDate();
-      result = num1 === num2 ? 0 : num1 > num2 ? 1 : -1;
-    }
-    return result;
+    return date1 != null && date2 != null
+      ? date1.getFullYear() - date2.getFullYear() || date1.getMonth() - date2.getMonth() || date1.getDate() - date2.getDate()
+      : date1 == null && date2 != null
+      ? -1
+      : 1;
+  }
+  /** Compare two dates (d1 < d2 = -1; d1 == d2 = 0; d1 > d2 = 1;) */
+  public static compareYearMonth(date1: Date | null | undefined, date2: Date | null | undefined): number {
+    return date1 != null && date2 != null
+      ? date1.getFullYear() - date2.getFullYear() || date1.getMonth() - date2.getMonth()
+      : date1 == null && date2 != null
+      ? -1
+      : 1;
+  }
+  /** Compare two dates (d1 < d2 = -1; d1 == d2 = 0; d1 > d2 = 1;) */
+  public static compareYear(date1: Date | null | undefined, date2: Date | null | undefined): number {
+    return date1 != null && date2 != null ? date1.getFullYear() - date2.getFullYear() : date1 == null && date2 != null ? -1 : 1;
   }
   /** Get the day number of the start of the week by locale. */
   public static getDayStartWeekByLocale(): number {
